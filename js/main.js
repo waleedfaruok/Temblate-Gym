a8c6b76dc378478bb40983a0b6c7c57e4aaf7e51
walid-faruok <wilamedo@gmail.com>
2021-12-30 17:45:12 -0800

    create project gym

---
+++ js/main.js
@@ -0,0 +1,82 @@
+// starte list
+let icon = document.querySelector(".iconse");
+let list = document.querySelector(".list");
+
+icon.addEventListener("click",function(){
+  if(list.style.display == "none"){
+    list.style.display = "block";
+  }else{
+    list.style.display = "none";
+  }
+});
+// end list
+
+// start cuonter
+let cont1 = document.querySelectorAll(".cont1 .cont");
+let conter = document.querySelector(".conter");
+let falsee = false;
+window.onscroll = function(){
+    if(window.scrollY >= 700){
+     if(!falsee){
+        cont1.forEach((col)=> stretenow(col));
+     }
+     falsee = true;
+    }
+};
+function stretenow(el){
+    let cont = el.dataset.cont;
+    let conte = setInterval(()=>{
+       el.textContent++;
+       if(el.textContent == cont){
+          clearInterval(conte);
+       }
+    },2000 / cont);
+   };
+  //  end cuonter
+  
+  //  start article
+   let list2 = document.querySelectorAll(".list2 li");
+   let elemnt = document.querySelectorAll(".elemnt > div");
+    
+ list2.forEach(li =>{
+   li.addEventListener("click",function(e) {
+        list2.forEach(ele =>{
+          ele.classList.remove("activ");
+        });
+           e.currentTarget.classList.add("activ");
+           elemnt.forEach(div =>{
+            div.style.display = 'none';
+        })
+        document.querySelector(e.currentTarget.dataset.cat).style.display = "block";
+   });
+ });
+//  end article
+
+//  start cuoente time
+ let data = new Date("Dec 31 2021 23:59:59").getTime();
+
+  setInterval(()=>{
+  let getdada = new Date().getTime();
+  let getDada = data - getdada; 
+  let days = Math.floor(getDada / (1000 * 60 * 60 * 24));
+  let hurs = Math.floor((getDada % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let munets = Math.floor((getDada % (1000 * 60 * 60)) / (1000 * 60));
+  let sucend = Math.floor((getDada % (1000 * 60)) / (1000));
+
+  document.querySelector(".cont0").innerHTML = days < 10 ? `0${days}` : days;
+  document.querySelector(".cont2").innerHTML = hurs < 10 ? `0${hurs}` : hurs;
+  document.querySelector(".cont3").innerHTML = munets < 10 ? `0${munets}` : munets;
+  document.querySelector(".cont4").innerHTML = sucend < 10 ? `0${sucend}` : sucend;
+
+},1000);
+//  end cuonte time
+
+// starte updowen 
+let up = document.querySelector(".btn-btnbro");
+up.onclick = function(){
+  window.scrollTo({
+    top:0,
+    behavior: "smooth",
+  });
+};
+// end updowen
